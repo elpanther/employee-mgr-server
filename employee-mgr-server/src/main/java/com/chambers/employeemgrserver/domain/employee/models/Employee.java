dc9dc765639fdf167fc69d1d956edf7fda1f802d
Jeff L <elpanther@verizon.net>
2022-08-16 22:19:58 -0400

    finished crud api

---
+++ employee-mgr-server/src/main/java/com/chambers/employeemgrserver/domain/employee/models/Employee.java
@@ -0,0 +1,38 @@
+package com.chambers.employeemgrserver.domain.employee.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // JPA to save to db
+@NoArgsConstructor // Non argument constructor
+@RequiredArgsConstructor// parameterized constructor
+@Data // getters and setters
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String email;
+
+    @Override
+    public String toString() {
+        return String.format("%d %s %s %s", id, firstName, lastName, email);
+
+    }
+}
